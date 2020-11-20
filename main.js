34a148c3eb0adc829c3fce038f1435a734d420da
varAdnez <varandez@gmail.com>
2020-11-21 00:36:15 +0300

    Улучшенный игровой процесс

--- main.js
+++ main.js
@@ -1,76 +1,143 @@
-const $btn = document.getElementById('btn-kick');
-const $fire = document.getElementById('btn-fire');
-const $ball = document.getElementById('btn-ball');
-
 const character = {
 	name: 'Pikachu',
-	defaultHP: 100,
-	damageHP: 100,
-	elHP: document.getElementById('health-character'),
-	elProgressbar: document.getElementById('progressbar-character'),
+	defaultHP: 150,
+	HP: 150,
+	elHPLabel: document.getElementById('health-character'),
+	elHPBar: document.getElementById('progressbar-character'),
+	getDamage,
+	renderHP,
+	renderHPLabel,
+	renderHPBar
+
 }
 
 const enemy = {
 	name: 'Charmander',
-	defaultHP: 100,
-	damageHP: 100,
-	elHP: document.getElementById('health-enemy'),
-	elProgressbar: document.getElementById('progressbar-enemy'),
+	defaultHP: 150,
+	HP: 150,
+	elHPLabel: document.getElementById('health-enemy'),
+	elHPBar: document.getElementById('progressbar-enemy'),
+	getDamage,
+	renderHP,
+	renderHPLabel,
+	renderHPBar
 }
 
-$btn.addEventListener('click', function () {
-	console.log('Kick');
-	changeHP(random(20), character);
-	changeHP(random(20), enemy);
-});
+const actions = [
+	{
+		elButton: document.getElementById('btn-kick'),
+		damageMultiplier: 30
+	},
+	{
+		elButton: document.getElementById('btn-kick-spec'),
+		damageMultiplier: 40
+	}
+]
 
-$fire.addEventListener('click', function () {
-	console.log('Kick');
-	changeHP(random(60), character);
-	changeHP(random(66), enemy);
-});
 
-$ball.addEventListener('click', function () {
-	console.log('Kick');
-	changeHP(random(99), character);
-	changeHP(random(99), enemy);
-});
-
-function init() {
-	console.log('Start game!');
-	renderHP(character);
-	renderHP(enemy);
+function renderHP() {
+	this.renderHPLabel();
+	this.renderHPBar();
 }
 
-function renderHP(person) {
-	renderHPlife(person);
-	renderProgressbarHP(person);
-
+function renderHPLabel() {
+	this.elHPLabel.innerText = this.HP + '/' + this.defaultHP;
 }
 
-function renderHPlife(person) {
-	person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+function renderHPBar() {
+	this.elHPBar.style.width = (this.HP * 100) / this.defaultHP + '%';
 }
 
-function renderProgressbarHP(person) {
-	person.elProgressbar.style.width = person.damageHP + '%';
-}
+function getDamage(count) {
+	const damageCount = Math.ceil((this.defaultHP / 100) * count);
 
-function changeHP(count, person) {
-	if (person.damageHP < count) {
-		person.damageHP = 0;
-		alert('Бедный ' + person.name + ' проиграл бой!');
-		$btn.disabled = true;
-	} else {
-		person.damageHP -= count;
+	this.HP -= damageCount;
+
+	console.log('HP ' + this.name + this.HP);
+	console.log('Earned damage: ' + Math.ceil((this.defaultHP / 100) * count));
+
+	if (count > (this.HP * 100) / this.defaultHP) {
+		this.HP = 0;
+
+		if (this.name === character.name) {
+
+			console.log(enemy.name + ' make last kick ' + '(' + damageCount + ') ' + 'to ' + character.name);
+		} else {
+			console.log(character.name + ' make last kick ' + '(' + damageCount + ') ' + 'to ' + enemy.name);
+		}
+
+		console.log('Бедный ' + this.name + ' проиграл бой!');
+		disableALlButtons(actions);
 	}
 
-	renderHP(person);
+	if (this.name === character.name) {
+		makeLog(character, enemy, damageCount);
+	} else {
+		makeLog(enemy, character, damageCount);
+	}
+
+
+
+	this.renderHP();
+
 }
 
 function random(num) {
-	return Math.ceil(Math.random() * num)
-
+	return Math.ceil(Math.random() * num);
 }
+
+function setupHitButtons(settings) {
+	for (let i = 0; i < settings.length; i++) {
+		settings[i].elButton.addEventListener('click', function () {
+			console.log('Kick');
+			character.getDamage(random(settings[i].damageMultiplier));
+			enemy.getDamage(random(settings[i].damageMultiplier));
+		});
+	}
+}
+
+function makeLog(target, person, damage) {
+	const consoleBar = document.getElementById('logs');
+	const consoleLog = document.createElement('p');
+	consoleLog.innerText = generateLog(target, person, damage);
+	consoleBar.insertBefore(consoleLog, consoleBar.children[0])
+}
+
+function disableALlButtons(buttons) {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].elButton.disabled = true;
+	}
+}
+
+function generateLog(firstPerson, secondPerson, damage) {
+	const logs = [
+		`${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`, // -12, [88/100]
+		`${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`,
+		`${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage}, [${firstPerson.HP}/${firstPerson.defaultHP}]`
+	];
+	return logs[random(logs.length - 1)];
+}
+
+function createConsole() {
+	const consoleBlock = document.createElement('div');
+	const playGround = document.querySelector('html');
+	playGround.appendChild(consoleBlock).setAttribute('id', 'logs');
+}
+
+function init() {
+	console.log('Start Game!');
+	character.renderHP();
+	enemy.renderHP();
+	setupHitButtons(actions);
+	createConsole();
+}
+
 
 init();
